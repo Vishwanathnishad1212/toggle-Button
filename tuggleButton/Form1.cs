487d88b5b376ae0c40581fe2e6d8a618b40c9775
Vishwanathnishad1212 <116053600+Vishwanathnishad1212@users.noreply.github.com>
2022-10-23 08:28:17 +0530

    Add files via upload

---
+++ tuggleButton/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tuggleButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        bool _perform = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_perform == false)
+            {
+                button1.Text = "Switch On";
+                pictureBox1.Image = Properties.Resources.SwitchOn;
+                _perform = true;
+            }
+            else
+            {
+                button1.Text = "Switch Off";
+                pictureBox1.Image = Properties.Resources.SwitchOff;
+                _perform = false;
+            }
+        }
+    }
+}
